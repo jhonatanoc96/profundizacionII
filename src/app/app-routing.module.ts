ff3a75b4c7581a54754c0c491304acfa0c056623
Jhonatan Ocampo <jhonatanocar96@gmail.com>
2020-04-02 22:16:33 -0500

    Cambiar nombre de componente browse

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
     { path: "", redirectTo: "/login", pathMatch: "full" },
     // { path: "home", loadChildren: () => import("~/app/home/home.module").then((m) => m.HomeModule) },
     { path: "home", loadChildren: "./home/home.module#HomeModule" },
-    { path: "browse", loadChildren: () => import("~/app/browse/browse.module").then((m) => m.BrowseModule) },
+    { path: "tipohabitacion", loadChildren: () => import("~/app/tipohabitacion/tipohabitacion.module").then((m) => m.TipohabitacionModule) },
     { path: "search", loadChildren: () => import("~/app/search/search.module").then((m) => m.SearchModule) },
     { path: "featured", loadChildren: () => import("~/app/featured/featured.module").then((m) => m.FeaturedModule) },
     { path: "settings", loadChildren: () => import("~/app/settings/settings.module").then((m) => m.SettingsModule) },
--- src/app/tipohabitacion/tipohabitacion.module.ts
+++ src/app/tipohabitacion/tipohabitacion.module.ts
@@ -1,19 +1,19 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
 
-import { BrowseRoutingModule } from "./browse-routing.module";
-import { BrowseComponent } from "./browse.component";
+import { TipohabitacionRoutingModule } from "./tipohabitacion-routing.module";
+import { TipohabitacionComponent } from "./tipohabitacion.component";
 
 @NgModule({
     imports: [
         NativeScriptCommonModule,
-        BrowseRoutingModule
+        TipohabitacionRoutingModule
     ],
     declarations: [
-        BrowseComponent
+        TipohabitacionComponent
     ],
     schemas: [
         NO_ERRORS_SCHEMA
     ]
 })
-export class BrowseModule { }
+export class TipohabitacionModule { }
