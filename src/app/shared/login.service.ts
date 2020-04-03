df7efddccc85e93d6ee614c91d089f9c8c8f546b
Noviembre2019 <pmarroyave66294@umanizales.edu.co>
2020-04-02 20:47:35 -0500

    Actualizar URL de la API

--- src/app/shared/login.service.ts
+++ src/app/shared/login.service.ts
@@ -5,7 +5,7 @@ import { HttpClient, HttpHeaders } from "@angular/common/http"
   providedIn: 'root'
 })
 export class LoginService {
-  private serverUrl = "https://apiprofjocampo.herokuapp.com/";
+  private serverUrl = "https://apiprof1parroyave.herokuapp.com/";
   private token: string;
 
   constructor(private http: HttpClient) { }
@@ -29,9 +29,9 @@ export class LoginService {
 
   }
 
-  // getHabitaciones() {
-  //   this.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZGU1YjE5YTlmODYxZDEyNjQwZTQxZjYiLCJpYXQiOjE1ODQ5MDkxNTcsImV4cCI6MTU4NDkxMDA1N30.wPpsBOg2BYbC8-cfJy5HU00VzbidaSY9kRt9iBQ3loU";
-  //   let headers = this.crearRequestHeader();
-  //   return this.http.get(this.serverUrl + "api/habitaciones", { headers });
+  getHabitaciones() {
+  this.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZGU1YjE5YTlmODYxZDEyNjQwZTQxZjYiLCJpYXQiOjE1ODQ5MDkxNTcsImV4cCI6MTU4NDkxMDA1N30.wPpsBOg2BYbC8-cfJy5HU00VzbidaSY9kRt9iBQ3loU";
+  let headers = this.crearRequestHeader();
+  return this.http.get(this.serverUrl + "api/habitaciones", { headers });
   // }
 }
